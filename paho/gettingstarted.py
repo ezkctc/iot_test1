1847921a333df72d44f1eeea88021e306decf04f
ezkctc <46705852+ezkctc@users.noreply.github.com>
2022-10-01 10:31:21 +0800

    working

--- paho/gettingstarted.py
+++ paho/gettingstarted.py
@@ -2,22 +2,26 @@ import paho.mqtt.client as mqtt
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
+
+
     print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
+    client.subscribe("$testtopic/#")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(msg.topic+" "+str(msg))
 
-client = mqtt.Client(client_id="clientId-E", clean_session=True, userdata=None, transport="tcp")
+client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("mqtt.eclipseprojects.io", 1883, 60)
-print('hi')
+client.username_pw_set("admin", "$7$101$rJ2xzRQELEL+tPqN$vrwtnSajWBmeiLwIjqMzBQkAPj74cLXuuewmUn5eL6Wtd7aCugkaiO5jJhDfyvFZyD7lX/SahiXZCLnf8SrqbQ==")
+
+client.connect("localhost", 1883,60)
+
 # Blocking call that     processes network traffic, dispatches callbacks and
 # handles reconnecting 
 # Other loop*() functions are available that give a threaded interface and a
